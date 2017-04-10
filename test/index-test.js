8519b932f44eec6ab99c446c8d6cff49903b3d67
Alice <amyalicedee@gmail.com>
2017-04-10 21:14:19 +0100

    Make it so

---
+++ test/index-test.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+
+describe('index.js', () => {
+
+  describe('newBoard', () => {
+    xit('creates a nested array', () => {
+      const board = newBoard(7, 7);
+    });
+  });
+});
+
---
+++ test/something-test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('a test', () => {
+  it('can add up', () => {
+    expect(1 + 1).to.equal(2);
+  });
+});
