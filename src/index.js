0a36f867f34b3dd0b32e4bd9c2d4debc5ed99e2d
Alice <amyalicedee@gmail.com>
2017-04-12 22:56:57 +0100

    Adds newBoard and addToken and tests

---
+++ src/index.js
@@ -0,0 +1,34 @@
+//this is the bit that holds the state
+
+function newBoard(x, y) {
+  // here we define a column as [] and we can push to it later
+  // const column = [] // will eventually be a max of x high
+  // const cols = Array.from(Array(y)).fill(1);
+
+  // below doesnt work because it fills array(y) with 8 pointers to column so if you change it (column) in one place later you will change them all, your datastructure is set now.
+  // const board = Array(y).fill(column);
+
+  //this works
+  const board = Array(y).fill(null).map(() => []);
+  return board;
+  // TODO: draw newBoard
+}
+
+function addToken(player, column, board) {
+  if (board[column-1].length > 6) {
+    return 'Fail. This column is full'
+  } else {
+    board[column-1].push(player);
+    // I'm mutating board with push so I dont need to re-return it
+    // return board;
+  }
+}
+
+// returns cell n of given column
+function cell(n, column) {
+  return column[n];
+}
+
+module.exports = { newBoard, addToken };
+
+// 1. a column is just an array.
--- test/index-test.js
+++ test/index-test.js
@@ -1,11 +1,59 @@
+require('../src/index.js');
+
 const expect = require('chai').expect;
+const { newBoard, addToken } = require('../src');
 
 describe('index.js', () => {
 
   describe('newBoard', () => {
-    xit('creates a nested array', () => {
-      const board = newBoard(7, 7);
+    it('creates a nested array', () => {
+      const board = newBoard(6, 7);
+      expect(board).to.deep.equal([
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+      ]);
+    });
+  });
+
+  describe('addToken', () => {
+    it('adds token to given column of given board', () => {
+      const board = newBoard(6, 7);
+      addToken('x', 3, board);
+      addToken('x', 3, board);
+      addToken('x', 3, board);
+      expect(board).to.deep.equal([
+        [],
+        [],
+        ['x', 'x', 'x'],
+        [],
+        [],
+        [],
+        [],
+      ]);
+    });
+
+    it('doesnt add token when column is full', () => {
+      const board = newBoard(6, 7);
+      addToken('x', 3, board);
+      addToken('x', 3, board);
+      addToken('x', 3, board);
+      addToken('o', 3, board);
+      addToken('o', 3, board);
+      addToken('o', 3, board);
+      addToken('x', 3, board);
+      expect(addToken('x', 3, board)).to.equal('Fail. This column is full');
     });
   });
 });
 
+
+// const board = newBoard(7,7);
+// addToken('x', 7, board);
+// addToken('x', 7, board);
+// addToken('x', 7, board);
+// addToken('x', 7, board);
